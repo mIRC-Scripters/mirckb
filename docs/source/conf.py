3a0de7fa14b50cd5e1e04a7507b45501f7119e8f
Sven Roelse <acvxqs@icloud.com>
2023-05-07 14:33:59 +0200

    Add directive for compatibility

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath("./_ext"))
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
@@ -33,6 +34,7 @@ extensions = [
 	'sphinx_rtd_theme',
 	'sphinx_tabs.tabs',
 	'sphinx_copybutton',
+	'mirckb_compat'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
